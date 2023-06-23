5fd0940a5e495b0a45d4c350849e73b8a11dc34a
IgorAlvesSG <126509797+IgorAlvesSG@users.noreply.github.com>
2023-06-23 15:44:14 -0300

    .

--- src/Pages/PoliticaPrivacidade/Section1PeP.jsx
+++ src/Pages/PoliticaPrivacidade/Section1PeP.jsx
@@ -1,18 +1,30 @@
 import style from './Section1PeP.module.css';
 
 function Section1PeP() {
-    return(
+    return (
         <div id={style.section1} >
-            <h3>Hitalo brocha</h3>
-            <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ratione fugiat quidem
-                 illum adipisci ipsam, iste eveniet, nemo saepe qui placeat cumque explicabo 
-                 assumenda, animi aut enim. Quam magnam quibusdam distinctio!</p>
-                 <div id={style.opções}>
-                    <details>
-                        <summary>Eureca na pepeca da Rebeca</summary>
-                        <p>Ah, é a vida</p>
-                    </details>
-                 </div>
+            <h3>atenção!</h3>
+            <p>Este é um aviso importante referente às políticas e privacidade do nosso site. Ao acessar e utilizar nossos serviços, você está concordando com os termos exemplificados abaixo:
+
+                Coleta de informações: Em algumas situações, podemos coletar informações pessoais dos usuários, como nome, endereço de e-mail, país e data de nascimento. Essas informações são coletadas com o objetivo de melhorar nossos serviços e garantir uma experiência mais personalizada para cada usuário.
+
+                Cookies: Nosso site utiliza cookies para aprimorar a navegação e personalizar o conteúdo mostrado aos usuários. Esses cookies são coletados automaticamente enquanto o usuário navega pelo site, mas podem ser desativados nas configurações do navegador.
+
+                Compartilhamento de informações: As informações coletadas dos usuários não serão divulgadas ou compartilhadas com terceiros, exceto em casos de ordem judicial ou relacionados à segurança do usuário e do site.
+
+                Segurança: Nosso site utiliza medidas de segurança para proteger as informações coletadas dos usuários, mas não nos responsabilizamos por eventuais violações de segurança que possam ocorrer.
+
+                Ao utilizar nossos serviços, o usuário concorda em seguir as políticas e privacidade aqui apresentadas. Em caso de dúvidas ou sugestões, por favor, entre em contato conosco.
+
+                Atenciosamente,<br />
+
+                Equipe Pecto</p>
+            <div id={style.opções}>
+                <details>
+                    <summary>Eureca na pepeca da Rebeca</summary>
+                    <p>Ah, é a vida</p>
+                </details>
+            </div>
         </div>
     )
 }
