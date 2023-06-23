fe99588dd105f666149ba11b903ad64325f677ad
IgorAlvesSG <126509797+IgorAlvesSG@users.noreply.github.com>
2023-06-23 14:02:32 -0300

    .

--- src/Pages/Faq/Faq.jsx
+++ src/Pages/Faq/Faq.jsx
@@ -5,12 +5,13 @@ import HeaderBlog from '../Home/HeaderBlog';
 import FooterBlog from '../Home/FooterBlog';
 
 import logo from '../../Images/logo.png';
+import Section1 from './Section1';
 
 function Faq() {
     return(
         <section id={style.politica_privacidade}>
             <HeaderBlog header_color='rgba(228, 239, 255, 0.95)' header_img={logo} />
-
+            <Section1/>
             <FooterBlog footer_img={logo} />
         </section>
     )
---
+++ src/Pages/Faq/Section1.jsx
@@ -0,0 +1,17 @@
+import style from './Section1.module.css';
+
+function Section1() {
+    return(
+        <div id={style.section1} >
+        
+                 <div id={style.opções}>
+                    <details>
+                        <summary>asdasdasdasdasd</summary>
+                        <p>Ah, é a vida</p>
+                    </details>
+                 </div>
+        </div>
+    )
+}
+
+export default Section1
