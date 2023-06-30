a043d8e571eacd6e8a3186dee949e9cc213d74c0
KetelynSousa <126901953+KetelynSousa@users.noreply.github.com>
2023-06-30 12:17:26 -0300

    Finalizado

--- src/Pages/Materias/MainMateria.jsx
+++ src/Pages/Materias/MainMateria.jsx
@@ -8,7 +8,7 @@ import Blog_PCD2 from '../../Images/Blog_PCD2.png';
 import Blog_PCD3 from '../../Images/Blog_PCD3.png';
 import Menina_PCD1 from '../../Images/Menina PCD_img1(2).png';
 import Menina_PCD2 from '../../Images/Menina PCD_img2.png';
-import crianças from '../../Images/crianças.jpeg';
+import criancas from '../../Images/crianças.jpeg';
 
 import lupa from '../../Images/Search_lupa.png';
 
@@ -19,11 +19,13 @@ function MainMateria(props) {
                 <div id={style.materia_part1}>
                     <div className={style.materia_topo}>
                         <h1>{props.titulo_materia}</h1>
-                        <h2 style={{backgroundColor: props.tema_cor, color: props.corFonte}}>{props.tema_materia}</h2>
+                        <h2 style={{backgroundColor: props.tema_cor, color: props.cor_fonte}}>{props.tema_materia}</h2>
                     </div>
                     <img src={props.materia_img} alt={props.materia_alt} />
                     <span>{props.publicacao_materia} | Pecto</span>
-                    <p>{props.materia_texto}</p>
+                    {props.materia_texto.map((texto, index) => (
+                          <p key={index}>{texto}</p>
+                    ))}
                 </div>
                 <div id={style.materia_part2}>
                     <div className={style.materia_topo}>
@@ -33,7 +35,17 @@ function MainMateria(props) {
                         </div>
                     </div>
                     <div>
-                        <Section2Blog blog_img={Blog_PCD1} blog_alt='Conclusão' />
+                        <Section2Blog blog_img={Blog_PCD1} blog_alt='Inclusão' background_color='#005BBB' text_color='#FFF' blog_theme="Inclusão"  />
+
+                        <Section2Blog blog_img={Blog_PCD2} blog_alt='Tecnologia'  background_color='#005BBB' text_color='#FFF' blog_theme="Tecnologia"  />
+
+                        <Section2Blog blog_img={Blog_PCD3} blog_alt='Comunicação' background_color='#005BBB' text_color='#FFF' blog_theme="Comunicação"  />
+                        
+                        <Section2Blog blog_img={Menina_PCD1} blog_alt='Inclusão' background_color='#005BBB' text_color='#FFF' blog_theme="Comportamento" />
+                        
+                        <Section2Blog blog_img={Menina_PCD2} blog_alt='Inclusão'  background_color='#005BBB' text_color='#FFF' blog_theme="Dificuldade" />
+
+                        <Section2Blog blog_img={criancas} blog_alt='Inclusão' background_color='#005BBB' text_color='#FFF' blog_theme="Acessibidade"  />
                     </div>
                 </div>
             </section>
