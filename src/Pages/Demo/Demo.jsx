2deeae9c973887863db82cfb949c6df580f4e8bc
hitalojacome <106604805+hitalojacome@users.noreply.github.com>
2023-07-03 13:22:50 -0300

    Reajustando mudança de imagem

--- src/Pages/Demo/Demo.jsx
+++ src/Pages/Demo/Demo.jsx
@@ -210,7 +210,7 @@ const Demo = () => {
 
                     {/*Menu Central*/}
 
-                    <section id={style.menu_central} style={{ backgroundImage: `${selectedVoice === 'Mileny' ? '' : `url('/static/media/Menino%20PCD_demo.0226535d7885090fe053.png')`}` }}>
+                    <section id={style.menu_central} className={`${selectedVoice === 'Mileny' ? '' : style.imagemMenino}`}>
                         <img src={logo} alt="logo da Pecto" />
                         <h2>Se comunique com quem quiser</h2>
                         <h3>{ cumprimento }</h3>
